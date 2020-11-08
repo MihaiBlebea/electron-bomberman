15da2349573f68befabc10e264507cf99e4bede2
Mihai Blebea <mihai@getchip.uk>
2020-11-08 16:36:41 +0000

    Draws the board. Don't look straight into it

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 const { ipcRenderer } = require('electron')
+const Board = require('./src/board')
 
 const myNotification = new Notification('Title', {
     body: 'Notification from the Renderer process'
@@ -16,10 +17,27 @@ window.addEventListener('offline', alertOnlineStatus)
 
 alertOnlineStatus()
 
-let buttonText = ''
 ipcRenderer.on('ping', (event, message) => {
-
-    buttonText += message
-    document.getElementById('app').innerHTML = buttonText
     console.log(message) // Prints 'whoooooooh!'
 })
+
+let board = new Board(100)
+board.render(document.getElementById('frame'))
+
+// let canvas = document.getElementById('display')
+// canvas.width = window.innerWidth
+// canvas.height = window.innerHeight
+
+// let rectX = 0;
+// let rectY = 0;
+
+// function draw(canvas) {
+//     // Clear the entire canvas
+//     let context = canvas.getContext("2d");
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+
+//     context.fillStyle = '#ff8080';
+//     context.fillRect(rectX, rectY, 150, 100);
+// }
+
+// draw(canvas)
--- src/board.js
+++ src/board.js
@@ -41,6 +41,42 @@ class Board
     {
         return this._boxSize
     }
+
+    render(anchorElement)
+    {
+        let canvas = this._createCanvas()
+        anchorElement.appendChild(canvas)
+    }
+
+    _createCanvas()
+    {
+        let canvas    = document.createElement("CANVAS")
+        canvas.width  = this._boxSize * this._boardSize
+        canvas.height = this._boxSize * this._boardSize
+
+        let context = canvas.getContext('2d')
+        
+        for (let x = 0; x < this._boardSize; x++) {
+            for (let y = 0; y < this._boardSize; y++) {
+                if (x % 2 === 0 && y % 2 === 0) {
+                    context.fillStyle = '#000'
+                } else {
+                    context.fillStyle = '#fff'
+                }
+
+                let startX = x * this._boxSize
+                let startY = y * this._boxSize
+                context.fillRect(startX, startY, startX + this._boxSize, startY + this._boxSize)
+            }
+        }
+
+        return canvas
+    }
+
+    update()
+    {
+
+    }
 }
 
 module.exports = Board
